1cc54b6265c5180202edb4cb92d1724a0e41efed
AhmadHasbyBik <abik262002@gmail.com>
2023-07-22 17:03:14 +0700

    add create, update, delete

--- controllers/phoneController/phoneController.go
+++ controllers/phoneController/phoneController.go
@@ -34,13 +34,46 @@ func GetById(c *gin.Context) {
 }
 
 func Create(c *gin.Context) {
+	var phone models.Phone
+	if err := c.ShouldBind(&phone); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
 
+	models.DB.Create(&phone)
+	c.JSON(http.StatusOK, gin.H{"phone": phone})
 }
 
 func Update(c *gin.Context) {
+	var phone models.Phone
+	id := c.Param("id")
 
+	if err := c.ShouldBind(&phone); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	if models.DB.Model(&phone).Where("id = ?", id).Updates(&phone).RowsAffected == 0 {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Cannot Update"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Data Has Been Updated"})
 }
 
 func Delete(c *gin.Context) {
+	var phone models.Phone
+	id := c.Param("id")
 
+	if err := c.ShouldBind(&phone); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	if models.DB.Delete(&phone, id).RowsAffected == 0 {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Cannot Delete"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Data Has Been deleted"})
 }
